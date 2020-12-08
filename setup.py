c32dff3f12ef887fde1e4d930ceec9cb1198d9a7
abdulsamedkeskin <erdemsametkeskin25@gmail.com>
2020-12-08 15:50:41 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import setuptools
+setup(
+  name = 'nested_loops',         
+  packages = setuptools.find_packages(),   
+  version = '1.0',    
+  license='MIT',      
+  description = 'Math Library For Python',   
+  author = 'Abdulsamet Keskin',                  
+  author_email = 'erdemsametkeskin25@gmail.com',      
+  url = 'https://github.com/abdulsamedkeskin/MathLibrary',   
+  download_url = 'https://github.com/abdulsamedkeskin/Nested-Loops/archive/V3.tar.gz',    
+  keywords = ['MathLibrary'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
