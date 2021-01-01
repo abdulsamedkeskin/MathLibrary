fe3519511276356494f13fafa52264d4134bf233
abdulsamedkeskin <erdemsametkeskin25@gmail.com>
2021-01-01 15:38:58 +0300

    added readme for PyPl

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 from distutils.core import setup
 import setuptools
-from setuptools import setup
 from os import path
 
-try:
-    readme = path.abspath(path.dirname(__file__))
-    with open(path.join(readme, 'Readme.md'), encoding='utf-8') as f:
-        readme_description = f.read()
-except:
-    pass
-try:
-    
-    setup(
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+
+setup(
       name = 'MathLibrary',         
       packages = setuptools.find_packages(),   
-      version = '9.0',
-      long_description=readme_description,
-      long_description_content_type='text/markdown',
+      version = '12.0',
       license='MIT',      
       description = 'Math Library For Python',   
       author = 'Abdulsamet Keskin',                  
       author_email = 'erdemsametkeskin25@gmail.com',      
       url = 'https://github.com/abdulsamedkeskin/MathLibrary',   
-      download_url = 'https://github.com/abdulsamedkeskin/MathLibrary/archive/V9.tar.gz',
+      download_url = 'https://github.com/abdulsamedkeskin/MathLibrary/archive/V12.tar.gz',
       keywords = ['MathLibrary'],
       classifiers=[
         'Development Status :: 3 - Alpha',      
@@ -34,6 +31,6 @@ try:
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
       ],
-    )
-except:
-    pass
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+)
